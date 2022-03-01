5052f646a3d9241ad594d5c5f379e60191ad57c4
PetraBlomqvist <93131052+PetraBlomqvist@users.noreply.github.com>
2022-03-01 14:00:31 +0100

    Add files via upload

--- part5.py
+++ part5.py
@@ -1,6 +1,11 @@
+#Tuples
+
+#List of numbers
 tuple=[54,67,43,89,21,3]
 
+#Sort numbers in order
 tuple.sort()
 
+#Print largest and smallest number to console
 print("Biggest number is: ",tuple[5])
 print("Smallest number is: ",tuple[0])
