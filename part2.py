0447d4ed1833c655086194b3ef77af0d1c019872
PetraBlomqvist <93131052+PetraBlomqvist@users.noreply.github.com>
2022-03-01 13:35:26 +0100

    Add files via upload

--- part2.py
+++ part2.py
@@ -1,9 +1,15 @@
+#Variables
+
+#Values to use,
 firstValue = 7
 secondValue = 3
 
+#Print values to console
 print(firstValue)
 print(secondValue)
 
+#Add values together
 firstValue = firstValue + secondValue
 
+#Print result of added values
 print(firstValue)
