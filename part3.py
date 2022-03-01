6fa053355a0701d6f5fd872261a8f12ebb54b220
PetraBlomqvist <93131052+PetraBlomqvist@users.noreply.github.com>
2022-03-01 11:02:49 +0100

    Add files via upload

---
+++ part3.py
@@ -0,0 +1,21 @@
+#Förlåt att jag inte ändrat något från det du gick igenom, men jag vill hoppa vidare till de andra sakerna <3
+
+#Function for adding two values together, prints and returns result
+def Additionfunction(A,B):
+    result = A+B
+    print(result)
+    return result 
+
+#Print the current value to the console
+def PrintCurrentValue():
+    print(CurrentValue)
+
+#Variable for keeping track of current value
+CurrentValue = 0
+
+
+CurrentValue = Additionfunction(1,3)
+CurrentValue = Additionfunction(5,1)
+CurrentValue = Additionfunction(6,7)
+
+PrintCurrentValue()
